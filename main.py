94c9434dc13a19f7faa2f93035005aafeec3e7d2
kwrzesni <kamil.wrzesniak@onet.pl>
2024-07-31 12:46:05 +0200

    dont play for Wisnia

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 bot = discord.Bot()
 channel_to_respond = {}
-
+wisnia_id = 447457072725098506
 
 def create_embed_from_song(song, title, author):
   seconds = song.length // 1000
@@ -40,6 +40,10 @@ async def connect_nodes():
 
 @bot.slash_command(name="play")
 async def play(ctx: discord.ApplicationContext, search: str):
+  if ctx.author.id == wisnia_id:
+    await ctx.send_response("Niech ci DjUwU zagra jak jesteś taki cwany")
+    return
+
   if ctx.author.voice is None:
     return await ctx.send_response("You must be in the voice channel.")
 
