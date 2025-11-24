a60332836140f78f924014a137ed4dc39ab1d08b
kamil <kamil.wrzesniak@onet.pl>
2025-11-25 00:54:00 +0100

    add prepare_enviroment.sh and remove requirements.txt

---
+++ prepare_enviroment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python -m venv SrodowiskoMuzyczne
+source SrodowiskoMuzyczne/bin/activate
+python -m pip install wavelink
+python -m pip install python-dotenv
+python -m pip install yt-dlp
+python -m pip install requests
+python -m pip install requests
+python -m pip install moviepy
+pythom -m pip install discord
+python -m pip install py-cord
